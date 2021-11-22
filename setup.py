e1ca98cc191d8f1e1b4575e0a3b87dfdf63420e3
Bendik R. Brenne <bendik@konstant.no>
2021-11-22 01:41:27 +0100

    Refactoring

--- setup.py
+++ setup.py
@@ -1,31 +1,7 @@
+"""Python package description."""
 from setuptools import setup
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
 setup(
-    name="podme_api-bendikrb",
-    version="0.0.2",
-    description="A client library for using the podme.com web API.",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/bendikrb/podme_api",
-    author="Bendik R. Brenne",
-    author_email="bendik@brenne.nu",
-    license="MIT",
-    packages=["podme_api"],
-    install_requires=[
-        "PyYAML",
-        "requests",
-        "youtube-dl",
-        "simplejson",
-    ],
-    classifiers=[
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-    ],
-    python_requires=">=3.6",
+    setup_requires=['pbr'],
+    pbr=True,
 )
---
+++ src/podme_api/__init__.py
@@ -0,0 +1,5 @@
+""" Init file for podme_api """
+
+import pkg_resources  # part of setuptools
+
+__version__ = pkg_resources.get_distribution("podme_api").version
